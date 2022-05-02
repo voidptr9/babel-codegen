831a30f4b4f4de49c102a40a068c49df5ae987e1
Abdullahi M <voidptr9@gmail.com>
2022-05-02 20:45:21 +0300

    feat: handle array holes, positional destructures

--- src/nodes/VariableDeclarator.js
+++ src/nodes/VariableDeclarator.js
@@ -1,6 +1,7 @@
 import * as t from "@babel/types";
 
 export default function VariableDeclarator(path) {
+  // Object destructure pattern.
   if (path.node.id.type == "ObjectPattern") {
     const vardecl = t.variableDeclaration(
       "var",
@@ -10,6 +11,41 @@ export default function VariableDeclarator(path) {
           t.memberExpression(path.node.init, key)
         );
       })
+    );
+
+    path.parentPath.replaceWith(vardecl);
+  }
+
+  // Array destructure pattern.
+  // We flatten the nodes by intentionally returning an array so that the correct indices
+  // can be used when some arrays are provided with default values, e.g:
+  //   =>   [i, j = 10, k] = getVectorCoordinates();
+  //   <=   var _ref = getVectorCoordinates();
+  //   <=   var i = _ref[0],
+  //            k = _ref[2];
+  //        j = _ref[1] || 10;
+  // Currently, the multiple calls to `getVectorCoordinates` are cached in a single scoped
+  // reference so computations happen exactly once.
+  if (path.node.id.type == "ArrayPattern") {
+    const $scopedIdent = path.scope.generateUidIdentifierBasedOnNode(
+      path.node.init
+    );
+    const scopedArrayDeclaration = t.variableDeclaration("var", [
+      t.variableDeclarator($scopedIdent, path.node.init),
+    ]);
+    path.parentPath.insertBefore(scopedArrayDeclaration);
+    const vardecl = t.variableDeclaration(
+      "var",
+      path.node.id.elements
+        .map(el => (el && el.type == "Identifier" ? el : undefined))
+        .map((ident, i) => {
+          if (!ident) return [];
+          return t.variableDeclarator(
+            ident,
+            t.memberExpression($scopedIdent, t.numericLiteral(i), true)
+          );
+        })
+        .flat(Infinity)
     );
 
     path.parentPath.replaceWith(vardecl);
--- test/index.js
+++ test/index.js
@@ -1,13 +1,28 @@
 import { babelCodegen } from "../src/index.js";
 import { transformSync } from "@babel/core";
+import cliTable from "cli-table";
+import cliHeighlight from "cli-highlight";
+
+const { highlight } = cliHeighlight;
+const Table = cliTable;
 
 const src = `const { self } = window;
 const x = { a: 1, b: 2 };
 const { a, b } = x;
-const [ k, v ] = x;`;
-
+const [ i, j, k ] = getVectorCoordinates();
+const [ , def_y = 20, z ] = value;`;
 const { code } = transformSync(src, {
   plugins: [babelCodegen],
 });
+const table = new Table({
+  head: ["Source", "Output"],
+  colWidths: [50, 70],
+});
 
-console.log(code);
+table.push([
+  highlight(src, { language: "javascript", ignoreIllegals: true }),
+  highlight(code, { language: "javascript", ignoreIllegals: true }),
+]);
+
+console.clear();
+console.log(table.toString());
